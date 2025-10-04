a7f6de2045d7b725447bf60965de42ceffa4bc80
k-nizy <k.nizeyiman@alustudent.com>
2025-10-04 09:06:56 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,38 +1,57 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
-using the function do_deploy
+Fabric script that distributes an archive to web servers
 """
-from fabric.api import env, run, put, hosts
-# from datetime import datetime
+
+from fabric.api import env, put, run, sudo
 import os
 
-env.hosts = ['34.74.23.57', '35.196.161.89']
+# Define your web servers here
+env.hosts = ['54.157.32.137', '52.55.249.213']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'  # Update with your SSH key path
 
 
 def do_deploy(archive_path):
     """
-    upload the archive to the /tmp/ directory of the web server
-    uncompress the archive
-    delete the archive from the web server
-    handle symbolic links
-    return True is all operations went well, False otherwise
+    Distributes an archive to web servers
     """
     if not os.path.exists(archive_path):
         return False
+
     try:
+        # Upload the archive to /tmp/ directory
         put(archive_path, '/tmp/')
-        file_name = archive_path.split('/')[-1]
-        file_name_noext = file_name.split('.')[0]
-        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
-        run('sudo mkdir -p {}'.format(new_folder))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
-        run('sudo rm /tmp/{}'.format(file_name))
-        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
-        run('sudo rm -rf {}web_static'.format(new_folder))
+
+        # Get archive filename without extension
+        archive_filename = os.path.basename(archive_path)
+        folder_name = archive_filename.replace('.tgz', '')
+        folder_path = '/data/web_static/releases/{}/'.format(folder_name)
+
+        # Create target directory
+        run('sudo mkdir -p {}'.format(folder_path))
+
+        # Uncompress archive to target directory
+        run('sudo tar -xzf /tmp/{} -C {}'.format(archive_filename, folder_path))
+
+        # Remove archive from /tmp/
+        run('sudo rm /tmp/{}'.format(archive_filename))
+
+        # Move contents to proper location
+        run('sudo mv {}web_static/* {}'.format(folder_path, folder_path))
+
+        # Remove empty web_static directory
+        run('sudo rm -rf {}web_static'.format(folder_path))
+
+        # Delete existing symbolic link
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
-        print("New version deployed!")
+
+        # Create new symbolic link
+        run('sudo ln -s {} /data/web_static/current'.format(folder_path))
+
+        print('New version deployed!')
         return True
-    except:
+
+    except Exception as e:
+        print("Deployment failed: {}".format(str(e)))
         return False
