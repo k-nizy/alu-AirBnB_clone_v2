db27d9aac46a0d726870ee51ae07035450600b69
k-nizy <k.nizeyiman@alustudent.com>
2025-10-09 02:33:43 +0200

    added the readme

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,22 +36,27 @@ def do_deploy(archive_path):
         run('mkdir -p /data/web_static/releases/{}/'.format(file_name_noext))
         
         # Uncompress the archive to the release directory
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, file_name_noext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            file_name, file_name_noext))
         
         # Delete the archive from the web server
         run('rm /tmp/{}'.format(file_name))
         
         # Move contents from web_static subdirectory to release directory
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(file_name_noext, file_name_noext))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            ' /data/web_static/releases/{}/'.format(
+            file_name_noext, file_name_noext))
         
         # Remove the now-empty web_static subdirectory
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name_noext))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            file_name_noext))
         
         # Delete the current symbolic link
         run('rm -rf /data/web_static/current')
         
         # Create new symbolic link to the new release
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(file_name_noext))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(
+            file_name_noext))
         
         print("New version deployed!")
         return True
